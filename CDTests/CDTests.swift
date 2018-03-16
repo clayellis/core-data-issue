7af1bf7b14b369e8c22d670b320104e268c7c719
Clay Ellis <clayellis@me.com>
2018-03-16 10:47:21 -0600

    Initial Commit

---
+++ CDTests/CDTests.swift
@@ -0,0 +1,36 @@
+//
+//  CDTests.swift
+//  CDTests
+//
+//  Created by Clay Ellis on 3/16/18.
+//  Copyright © 2018 Clay Ellis. All rights reserved.
+//
+
+import XCTest
+@testable import CD
+
+class CDTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
