5cf3f3b96d17708e2064d8421922cdc38e690ca7
Clay Ellis <clayellis@me.com>
2018-03-19 11:23:06 -0600

    Add close() (to close out the stack without getting errors in testing). Update Contextual and Container stacks. Pass store type in init.

--- CD/CoreDataStack.swift
+++ CD/CoreDataStack.swift
@@ -10,11 +10,12 @@ import Foundation
 import CoreData
 
 protocol CoreDataStackProtocol {
-    init(modelName: String, url: URL?)
+    init(modelName: String, url: URL?, type: String)
     var viewContext: NSManagedObjectContext { get }
     var backgroundContext: NSManagedObjectContext { get }
     func performBackgroundTask(_ task: @escaping (NSManagedObjectContext) -> Void)
     func loadStore(_ completion: @escaping (Error?) -> Void)
+    func close()
 }
 
 class ContainerStack: CoreDataStackProtocol {
@@ -30,10 +31,11 @@ class ContainerStack: CoreDataStackProtocol {
         return context
     }
 
-    required init(modelName: String, url: URL? = nil) {
+    required init(modelName: String, url: URL? = nil, type: String) {
         let description = NSPersistentStoreDescription()
         description.shouldAddStoreAsynchronously = true
         description.url = url
+        description.type = type
 
         container = NSPersistentContainer(name: modelName)
         container.persistentStoreDescriptions = [description]
@@ -53,20 +55,41 @@ class ContainerStack: CoreDataStackProtocol {
     }
 
     func performBackgroundTask(_ task: @escaping (NSManagedObjectContext) -> Void) {
-        container.performBackgroundTask { context in
-            self.configure(context: context)
+        let context = container.newBackgroundContext()
+        configure(context: context)
+        context.performAndWait {
             task(context)
         }
+//        container.performBackgroundTask { context in
+//            self.configure(context: context)
+//            task(context)
+//        }
     }
 
     private func configure(context: NSManagedObjectContext) {
         context.automaticallyMergesChangesFromParent = true
-        context.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
+//        context.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
+//        context.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        context.mergePolicy = NSOverwriteMergePolicy
+    }
+
+    func close() {
+        for store in container.persistentStoreCoordinator.persistentStores {
+            guard let url = store.url else {
+                continue
+            }
+            do {
+                try container.persistentStoreCoordinator.destroyPersistentStore(at: url, ofType: store.type, options: nil)
+            } catch {
+                print(error.humanReadableString)
+            }
+        }
     }
 }
 
 class ContextualStack: CoreDataStackProtocol {
 
+    private let coordinator: NSPersistentStoreCoordinator
     private let _storeContext: NSManagedObjectContext
     private let _viewContext: NSManagedObjectContext
     private let _backgroundContext: NSManagedObjectContext
@@ -76,16 +99,17 @@ class ContextualStack: CoreDataStackProtocol {
     }
 
     var backgroundContext: NSManagedObjectContext {
-        return _backgroundContext
+        return newBackgroundContext()
+//        return _backgroundContext
     }
 
-    required init(modelName: String, url: URL? = nil) {
+    required init(modelName: String, url: URL? = nil, type: String) {
         let model = NSManagedObjectModel.mergedModel(from: [.main])!
-        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
+        coordinator = NSPersistentStoreCoordinator(managedObjectModel: model)
         let options = [NSMigratePersistentStoresAutomaticallyOption: true,
                        NSInferMappingModelAutomaticallyOption: true]
 
-        try! coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: options)
+        try! coordinator.addPersistentStore(ofType: type, configurationName: nil, at: url, options: options)
 
         if let storeURL = coordinator.persistentStores.first?.url {
             print("Added store at: \(storeURL)")
@@ -93,7 +117,9 @@ class ContextualStack: CoreDataStackProtocol {
 
         _storeContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
         _storeContext.persistentStoreCoordinator = coordinator
-        _storeContext.mergePolicy = NSMergeByPropertyStoreTrumpMergePolicy
+        _storeContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+//        _storeContext.mergePolicy = NSOverwriteMergePolicy
+//        _storeContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
 
         _viewContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
         _viewContext.parent = _storeContext
@@ -106,6 +132,13 @@ class ContextualStack: CoreDataStackProtocol {
 
     func loadStore(_ completion: @escaping (Error?) -> Void) {
         completion(nil)
+    }
+
+    private func newBackgroundContext() -> NSManagedObjectContext {
+        let context = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        context.parent = _viewContext
+        context.mergePolicy = _viewContext.mergePolicy
+        return context
     }
 
     func performBackgroundTask(_ task: @escaping (NSManagedObjectContext) -> Void) {
@@ -128,6 +161,19 @@ class ContextualStack: CoreDataStackProtocol {
                 try context.save()
             } catch {
                 print("Failed to save \(name) context: \(error.humanReadableString)")
+            }
+        }
+    }
+
+    func close() {
+        for store in coordinator.persistentStores {
+            guard let url = store.url else {
+                continue
+            }
+            do {
+                try coordinator.destroyPersistentStore(at: url, ofType: store.type, options: nil)
+            } catch {
+                print(error.humanReadableString)
             }
         }
     }
